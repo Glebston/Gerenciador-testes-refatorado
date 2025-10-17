aeca94375b48d650ae74df75d4bb5fcc16d06dd1
GL <saianolucrobr@gmail.com>
2025-10-17 16:38:11 -0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -948,3 +948,13 @@ export const openOptionsModal = (type, options) => {
     ).join('');
     DOM.optionsModal.classList.remove('hidden');
 };
+
+// **NOVA FUNÇÃO**
+// Função para aplicar máscara de telefone (xx) xxxxx-xxxx
+export const formatPhoneNumber = (value) => {
+    if (!value) return "";
+    value = value.replace(/\D/g,'');             // Remove tudo o que não é dígito
+    value = value.replace(/^(\d{2})(\d)/g,'($1) $2'); // Coloca parênteses em volta dos dois primeiros dígitos
+    value = value.replace(/(\d)(\d{4})$/,'$1-$2');    // Coloca hífen entre o quarto e o quinto dígitos
+    return value;
+}
