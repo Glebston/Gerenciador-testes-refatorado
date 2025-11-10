240be9afbaab4312bf1023385da2d17307dca40c
GL <saianolucrobr@gmail.com>
2025-11-10 11:19:16 -0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -5,43 +5,25 @@
 // ESTE ARQUIVO NÃO CONTÉM LÓGICA.
 // ==========================================================
 
-// 1. IMPORTA OS ESPECIALISTAS
+// Reexporta tudo dos especialistas
 
-// Importa seletores do DOM e constantes
-import { DOM, SIZES_ORDER, CHECK_ICON_SVG } from './ui/dom.js';
+// Exporta seletores do DOM e constantes
+export * from './ui/dom.js';
 
-// Importa funções de modais (showInfoModal, etc.)
-import * as Modals from './ui/modalHandler.js';
+// Exporta funções de modais (showInfoModal, etc.)
+export * from './ui/modalHandler.js';
 
-// Importa renderizadores do financeiro (renderFinanceDashboard, etc.)
-import * as FinanceUI from './ui/financeRenderer.js';
+// Exporta renderizadores do financeiro (renderFinanceDashboard, etc.)
+export * from './ui/financeRenderer.js';
 
-// Importa renderizadores de pedidos (renderOrders, addOrderCard, etc.)
-import * as OrderUI from './ui/orderRenderer.js';
+// Exporta renderizadores de pedidos (renderOrders, addOrderCard, etc.)
+export * from './ui/orderRenderer.js';
 
-// Importa manipuladores de formulário (addPart, updateFinancials, etc.)
-import * as FormHandler from './ui/formHandler.js';
+// Exporta manipuladores de formulário (addPart, updateFinancials, etc.)
+export * from './ui/formHandler.js';
 
-// Importa renderizadores da tabela de preços (renderPriceTable, etc.)
-import * as PriceTableUI from './ui/priceTableRenderer.js';
+// Exporta renderizadores da tabela de preços (renderPriceTable, etc.)
+export * from './ui/priceTableRenderer.js';
 
-// Importa funções ajudantes (formatPhoneNumber, updateSourceSelectionUI, etc.)
-import * as Helpers from './ui/helpers.js';
-
-
-// 2. REEXPORTA TUDO PARA O RESTANTE DA APLICAÇÃO (ex: main.js)
-
-export {
-    // Constantes e DOM
-    DOM,
-    SIZES_ORDER,
-    CHECK_ICON_SVG,
-    
-    // Módulos
-    ...Modals,
-    ...OrderUI,
-    ...FinanceUI,
-    ...FormHandler,
-    ...PriceTableUI,
-    ...Helpers
-};
+// Exporta funções ajudantes (formatPhoneNumber, updateSourceSelectionUI, etc.)
+export * from './ui/helpers.js';
